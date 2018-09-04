74756a3e3dcd095c9ab8904fb5dc3262225d37a4
Thomas Kilgour <t.kilgour@gmail.com>
2018-09-04 13:59:19 -0400

    update to localhost

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "https://quiz.thomaskilgour.ca/api"
+  baseURL: "http://localhost:8081/api"
 });
 
 apiInstance.interceptors.request.use(config => {
