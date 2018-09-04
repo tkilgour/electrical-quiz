6eac9ed14f261e4964338f001e1be4867c42f296
Thomas Kilgour <t.kilgour@gmail.com>
2018-09-04 17:13:59 -0400

    update comment rendering

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -5,7 +5,8 @@
       <input type="radio" :id="answerObj.id.concat(data._id)" :value="answerObj.id" v-model="selected" @click="$emit('answerSelect', data._id, answerObj.id)">
       <label :for="answerObj.id.concat(data._id)"> {{answerObj.answer}}</label>
     </div>
-    <p v-if="data.correct != null"><strong>Selected:</strong><span :class="data.correct ? 'correct' : 'incorrect'">{{ data.comment }}</span></p>
+    <p v-if="data.correct != null"><span :class="data.correct ? 'correct' : 'incorrect'">{{ data.correct ? 'Correct!' : 'Incorrect'}}</span></p>
+    <div v-if="data.correct === false" class="comment" v-html="data.comment"></div>
   </div>
 </template>
 
@@ -38,5 +39,26 @@ export default {
 
 .incorrect::before {
   content: " ✗ ";
+}
+
+.comment {
+  border: #aaa 1px solid;
+  border-radius: 0.2em;
+  background-color: #eee;
+  padding: 1em;
+  padding-left: 2.5em;
+  position: relative;
+  font-style: italic;
+}
+
+.comment::before {
+  content: "!";
+  color: #f52020;
+  position: absolute;
+  left: 0.8em;
+  font-size: 1.5em;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 1;
 }
 </style>
