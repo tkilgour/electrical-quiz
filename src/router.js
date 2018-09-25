f9da056c20e96e8a23fe6ec3faf135ee0c3e1453
Thomas Kilgour <t.kilgour@gmail.com>
2018-09-25 14:15:35 -0400

    remove code splitting

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import StartScreen from './components/StartScreen.vue'
+import Quiz from './components/Quiz.vue'
 
 Vue.use(Router)
 
@@ -16,10 +17,7 @@ export default new Router({
     {
       path: '/quiz',
       name: 'quiz',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './components/Quiz.vue')
+      component: Quiz
     }
   ]
 })
